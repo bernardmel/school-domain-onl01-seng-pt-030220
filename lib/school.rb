d031d2c432c388105eef786d514f737a53a92c1f
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-18 17:55:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,5 +10,11 @@ class School
     @roster
   end
 
-
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name 
 end
